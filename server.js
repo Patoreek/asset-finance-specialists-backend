3c4bc17034ab6162fa85ccef4c97cfd4eb4654b1
Patrick Minda <patrick.minda@hotmail.com>
2024-12-18 09:35:14 +1100

    Cleanup

--- server.js
+++ server.js
@@ -2,20 +2,18 @@ require("dotenv").config();
 const express = require("express");
 const connectMongoDB = require("./config/db");
 
-require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 app.use(express.json());
 connectMongoDB();
 
-// Example route
+app.use("/api/auth", authRoutes);
+
 app.get("/", (req, res) => {
   res.send("Hello, Express with MongoDB!");
 });
 
-// Start the server
 const PORT = process.env.PORT || 6000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
